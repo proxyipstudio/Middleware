979a9999318656437ae629b4a49da0dad51bc459
proxyipstudio <m13750670674@163.com>
2018-01-24 22:59:03 +0800

    1
    1

---
+++ Middleware/Middleware/Class1.cs
@@ -0,0 +1,29 @@
+﻿#region 文件信息
+
+/* ======================================================================== 
+
+* 【本类功能概述】 
+
+*  
+
+* 作者：献丑　　　　　　　　　时间：2018/1/24 22:58:44 
+
+* 文件名：Class1 
+
+* 版本：V1.0.0 
+
+========================================================================  */
+
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Middleware
+{
+    class Class1
+    {
+    }
+}
